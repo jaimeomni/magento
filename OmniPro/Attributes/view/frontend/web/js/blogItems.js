e76f616e29e28adc50a17418964c48ce92131172
jaimeomni <jaime.santana@omni.pro>
2021-04-14 16:56:29 -0500

    Se agrega modulo de blog sin requirejs

--- OmniPro/Attributes/view/frontend/web/js/blogItems.js
+++ OmniPro/Attributes/view/frontend/web/js/blogItems.js
@@ -1,22 +1,26 @@
 define([
     'uiComponent',
     'ko',
-    'jquery'
-], function(Component, ko, $) {
+    'jquery',
+    'mage/storage'
+], function(Component, ko, $, storage) {
     return Component.extend({
         defaults: {
+            blogs: [],
+            blogsUrl: '/rest/V1/blogs?searchCriteria',
             textoPrueba: "Texto Prueba",
             variable1: 25
         },
         initialize: function() {
             this._super();
+            this.getBlogs();
             setTimeout($.proxy(function() {
                 this.textoPrueba("Prueba 2");
                 console.log(this);
             }, this), 1000)
-            var blogs = "/rest/V1/blogs?searchCriteria";
+            var blogs1 = "/rest/V1/blogs?searchCriteria";
             $.ajax({
-                url: blogs
+                url: blogs1
             }).done(function (response) {
                 console.log(response);
             });
@@ -27,12 +31,21 @@ define([
                 .observe([
                     'variable1',
                     'textoPrueba'
-                ]);
+                ])
+                .observe({
+                    blogs: []
+                });
 
             return this;
         },
         cambiarVariable: function() {
             this.variable1(0);
+        },
+        getBlogs: function() {
+            storage.get(this.blogsUrl)
+            .then($.proxy(function(data) {
+                this.blogs(data.items);
+            },this));
         }
     });
 });
