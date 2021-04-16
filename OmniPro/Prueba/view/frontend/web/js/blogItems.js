f888f7434ad2b6a1b7662aabf56b7b8b8ea49ba7
jaimeomni <jaime.santana@omni.pro>
2021-04-16 09:33:47 -0500

    Se decodifica imagen en base64 para guardarla

--- OmniPro/Prueba/view/frontend/web/js/blogItems.js
+++ OmniPro/Prueba/view/frontend/web/js/blogItems.js
@@ -3,14 +3,17 @@ define([
     'ko',
     'jquery',
     'mage/storage',
-    'mage/url'
-], function (Component, ko, $, storage) {
+    'mage/url',
+    'mage/validation'
+], function (Component, ko, $, storage, url) {
+    url.setBaseUrl(window.BASE_URL);
     return Component.extend({
         defaults: {
             titulo: '',
             contenido: '',
             email: '',
             image: '',
+            imageBase64: '',
             blogs: [],
             blogsUrl: '/rest/V1/blogs?searchCriteria',
             blogPostUrl: 'rest/V1/blogs'
@@ -35,7 +38,8 @@ define([
                     'titulo',
                     'contenido',
                     'email',
-                    'image'
+                    'image',
+                    'imageBase64'
                 ])
                 .observe({
                     blogs: []
@@ -43,19 +47,36 @@ define([
 
             return this;
         },
-        sendBlog: function () {
+        isFormValid: function (form) {
+            return $(form).validation() &&  $(form).validation('isValid');
+        },
+        changeImage: function (data, event) {
+            var image = event.target.files[0];
+            var reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = $.proxy(function(e) {
+                this.imageBase64(reader.result);
+            }, this);
+        },
+        sendBlog: function (form) {
+            if (!this.isFormValid(form)) {
+                return;
+            }
             var blog = {
                 'blog': {
                     "title": this.titulo(),
                     "email": this.email(),
                     "content": this.contenido(),
-                    "img": this.image()
+                    "img": this.imageBase64()
                 }
             };
             storage.post(this.blogPostUrl, JSON.stringify(blog))
                 .then($.proxy(function () {
                     this.getBlogs();
                 }, this));
+            this.titulo('');
+            this.contenido('');
+            this.email('');
         },
         getBlogs: function () {
             storage.get(this.blogsUrl)
